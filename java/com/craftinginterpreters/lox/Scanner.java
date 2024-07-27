997eba25c2a372cd1561e640849f3bff986d2652
dasJake <dev.die.das.jake@gmail.com>
2024-07-28 00:40:11 +0200

    add scanToken;chap4.5.1beg

--- java/com/craftinginterpreters/lox/Scanner.java
+++ java/com/craftinginterpreters/lox/Scanner.java
@@ -24,9 +24,39 @@ class Scanner {
             start = current;
             scanToken();
         }
+    }
+
+    private void scanToken() {
+        char c = advance();
+        swith (c) {
+            case '(': addToken(LEFT_PAREN); break;
+            case ')': addToken(RIGHT_PAREN); break;
+            case '{': addToken(LEFT_BRACE); break;
+            case '}': addToken(RIGHT_BRACE); break;
+            case ',': addToken(COMMA); break;
+            case '.': addToken(DOT); break;
+            case '-': addToken(MINUS); break;
+            case '+': addToken(PLUS); break;
+            case ';': addToken(SEMICOLON); break;
+            case '*': addToken(STAR); break;
+        }
+    }
 
     private boolean isAtEnd() {
         return current >= source.length();
+    }
+
+    private char advance() {
+        return source.charAt(current++);
+    }
+
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
     }
 
     tokens.add(new Token(EOF, ""; null, line));
