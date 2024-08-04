e7266b64dc344cbb641b03f4a7c287efdee16e6c
dasJake <72259780+dasJake@users.noreply.github.com>
2024-08-04 18:27:10 +0200

    update personal notes

--- java/com/craftinginterpreters/lox/notes.Scanner.java
+++ java/com/craftinginterpreters/lox/notes.Scanner.java
@@ -10,13 +10,34 @@ import static com.craftinginterpreters.lox.TokenType.*;
 class Scanner {
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
-//JAKE NOTE: try saving corrent char instead of just ints
+//JAKE NOTE: try saving current char instead of just ints
     private int start = 0;
     private int current = 0;
     private int line = 1;
 
-    Scanner(String source){
+    Scanner(String source) {
         this.source = source;
+
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new Hashmap<>();
+        keywords.put("and",     AND);
+        keywords.put("class",   CLASS);
+        keywords.put("else",    ELSE);
+        keywords.put("false",   FALSE);
+        keywords.put("for",     FOR);
+        keywords.put("fun",     FUN);
+        keywords.put("if",      IF);
+        keywords.put("nil",     NIL);
+        keywords.put("or",      OR);
+        keywords.put("print",   PRINT);
+        keywords.put("return",  RETURN);
+        keywords.put("super",   SUPER);
+        keywords.put("this",    THIS);
+        keywords.put("true",    TRUE);
+        keywords.put("var",     VAR);
+        keywords.put("while",   WHILE);
     }
 
     List<Token> scanTokens() {
@@ -81,23 +102,37 @@ class Scanner {
             default:
                 if (isDigit(c)) {
                     number();
+                }
+                else if (isAlpha(c)) {
+                    identifier();
+                }
                 } else {
                     Lox.error(line, "Unexpected character.");
                 }
                 break;
         }
     }
+    private void identifier() {
+        while (isAlphaNumeric(peek())) advance();
+
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type == null) type =  IDENTIFIER;
+        addToken(type);
+    }
 
     private void number() {
-        while(isDigit(peek())) advance();
+        while (isDigit(peek())) advance();
 
         // Look for a fractional part.
         if (peek() == '.' && isDigit(peekNext())) {
             // Consume the "."
             advance();
 
-            while(isDigit(peek())) advance();
+            while (isDigit(peek())) advance();
         }
+//JAKE note: could the second while also come here outside the if?
+// it should work exactly the same
 
         addToken(NUMBER,
             Double.parseDouble(source.substring(start, current)));
@@ -141,6 +176,16 @@ class Scanner {
     private char peekNext() {
         if (current +1 >= source.length()) return '\0';
         return source.charAt(current + 1);
+    }
+
+    private boolean isAlpha(char c) {
+        return  (c >= 'a' && c <= 'z') ||
+                (c >= 'A' && c <= 'Z') ||
+                 c == '_';
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
     }
 
     private boolean isDigit(char c) {
